b618e24dd843d81b1d252678c54fd9f67ba68852
anonymous anonymous <anum.mithani@kellogg.northwestern.edu>
2022-02-18 20:19:56 +0000

    Generate graphiti resources for items

---
+++ app/resources/item_resource.rb
@@ -0,0 +1,17 @@
+class ItemResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :price, :string
+  attribute :description, :string
+  attribute :category_id, :integer
+  attribute :location, :string
+  attribute :image, :string
+  attribute :user_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
